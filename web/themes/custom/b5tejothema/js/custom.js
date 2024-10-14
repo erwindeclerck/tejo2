155507f5b3f9ce5d642f32ddfcd31f591a557955
Erwin De Clerck <erwin.de.clerck@gmail.com>
2024-10-15 01:19:44 +0200

    leaflet

--- web/themes/custom/b5tejothema/js/custom.js
+++ web/themes/custom/b5tejothema/js/custom.js
@@ -3,6 +3,7 @@ jQuery(" a").each(function () {
   //this part works when url matches
   if (this.href === window.location.href) {
     jQuery(this).addClass("active-link");
+    jQuery(this).addClass("dronken-link");
   }
 });
 
@@ -22,8 +23,9 @@ jQuery(".search-block-form input").blur(function(event){
   jQuery(".form-type-search").removeClass("sbox");
 });
 
-
+/*
 function wrap(selector, wrapper) {
+  var el;
   el = document.querySelector(selector);
   el.parentNode.insertBefore(wrapper, el);
   wrapper.appendChild(el);
@@ -31,3 +33,45 @@ function wrap(selector, wrapper) {
 
 // example: wrapping an anchor with class "wrap_me" into a new div element
 wrap(document.querySelector('a.wrap_me'), document.createElement('div'));
+*/
+
+
+jQuery(document).bind('leaflet.map', function(event, map, lMap) {
+  //map id
+  var id = lMap._container.id;
+  var url = window.location.href;
+  var settingsElement = document.querySelector('script[type="application/json"][data-drupal-selector="drupal-settings-json"]');
+  var drupalSettings = JSON.parse(settingsElement.textContent);
+  var leafletset = drupalSettings.leaflet;
+  var length = drupalSettings.leaflet[ id ].features.length;
+  var target =  jQuery('#printer');
+  var lat, lon, name, li;
+  var listing = "<ul>";
+  jQuery.each(leafletset,function(key,value){
+    lat = value.features[0].lat;
+    lon = value.features[0].lon;
+    name = value.features[0].popup.value;
+  listing += "<li data-id=" + key +" data-lat=" + lat + " data-lon=" + lon + ">" + name + "</li>";
+  });
+  listing += "</ul>";
+  jQuery(target).html(listing);
+  var lijst = jQuery("#printer ul li");
+  lijst.each(
+    function(){
+      jQuery(this).hover(
+        function(){
+          jQuery(this).css('text-decoration','underline');
+          jQuery(this).css('cursor','hand');
+          lMap.setZoom(15);
+          lMap.flyTo([this.dataset.lat,this.dataset.lon],14,{animate:true,duration:1.5});
+        },
+      function(){
+        jQuery(this).css('text-decoration','none');
+        jQuery(this).css('cursor','pointer');
+      }
+      );
+    }
+  );
+
+
+});
